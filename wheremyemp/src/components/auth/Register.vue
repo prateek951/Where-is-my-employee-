fc557e2a7ec795353292857bda52e2552019b0ad
prateek951 <prince.cenation@gmail.com>
2018-07-14 02:00:57 +0530

    registration working

---
+++ wheremyemp/src/components/auth/Register.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="register container">
+        <form class="card-panel" @submit.prevent="doRegister()">
+            <h2 class="center deep-purple-text">Register</h2>
+            <div class="field">
+                <label for="email">Email:</label>
+                <input type="email" placeholder="Email" v-model="email">
+            </div>
+            <div class="field">
+                <label for="password">Password:</label>
+                <input type="password" placeholder="Password" v-model="password">
+            </div>
+            <div class="field">
+                <label for="name">Name:</label>
+                <input type="text" placeholder="Name" v-model="name">
+            </div>
+            <div class="field center">
+                <button class="btn deep-purple">Register</button>
+            </div>
+        </form>
+    </div>    
+</template>
+
+
+<script>
+    export default{
+        name: 'Register',
+        data(){
+            return {
+                email: '',
+                password: '',
+                name: ''
+            }
+        },
+        methods: {
+            doRegister(){
+                console.log('inside the doRegister call....');
+            }
+        }
+    }
+</script>
+
+<style>
+.register{
+    max-width: 400px;
+    margin-top: 60px;
+}
+.register h2{
+    font-size: 2.4em;
+}
+.register .field{
+    margin-bottom: 16px;
+}
+</style>
--- wheremyemp/src/router/index.js
+++ wheremyemp/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Map from '@/components/Map/Map';
+import Register from '@/components/auth/Register';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,11 @@ export default new VueRouter({
             path: '/',
             name: 'Map',
             component: Map        
+        },
+        {
+            path: '/register',
+            name: 'Register',
+            component: Register       
         }
     ]
 })
