e2bfe059f959980e9ddd1fdf7c2d5c30681dfd71
prateek951 <prince.cenation@gmail.com>
2018-07-21 11:38:44 +0530

    working on adding the comments

--- wheremyemp/src/components/Profile/ViewProfile.vue
+++ wheremyemp/src/components/Profile/ViewProfile.vue
@@ -2,6 +2,16 @@
     <div class="view-profile container">
         <div v-if="profile" class="card">
             <h2 class="deep-purple-text center">{{profile.username}}'s Wall</h2>        
+            <ul class="comments collection">
+                <li class="collection-item">Comments</li>
+            </ul>
+            <form @submit.prevent="onAddComment()">
+                <div class="field">
+                    <label for="comment">Add a comment</label>
+                    <input type="text" name="comment" v-model="newcom">
+                    <p v-if="feedback" class="red-text center">{{ feedback }}</p>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -13,7 +23,17 @@ export default {
     name: 'ViewProfile',
     data(){
         return {
-           profile:null 
+           profile:null,
+           newcom: null,
+           feedback: null 
+        }
+    },
+    methods: {
+        onAddComment(){
+            console.log('inside the add comment method....');
+            if(this.newcom === null || this.newcom === '') {
+                this.feedback = 'Please add a comment'
+            }
         }
     },
     created() {
